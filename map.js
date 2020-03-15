86417508f3aae6d80646eb9388e0ea6a0e8d0b70
Mark Reckard <mark.reckard@gmail.com>
2020-03-15 12:51:06 -0400

    IMPROVE: move everything to separate js file and add loadlistener for bounds

---
+++ map.js
@@ -0,0 +1,43 @@
+let map;
+let allKmls = [];
+
+function initMap() {
+  const mapOptions = {
+    zoom: 6,
+    center: new google.maps.LatLng(46.908312, -124.112932)
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  kmlImport();
+  addLoadListener();
+}
+
+function kmlImport() {
+  fileNames = ['5-30--6-7', '6-8--6-18', '6-19--6-28'];
+  fileNames.forEach(fileName => {
+    const src = `https://raw.githubusercontent.com/marker004/bike-trip/master/kmls/${fileName}.kml`;
+
+    const kml = new google.maps.KmlLayer(src, {
+      suppressInfoWindows: false,
+      preserveViewport: false,
+      map: map
+    });
+
+    allKmls.push(kml)
+  })
+}
+
+function addLoadListener() {
+  google.maps.event.addListenerOnce(map, 'tilesloaded', () => {
+    let totalBounds = new google.maps.LatLngBounds
+    console.log(allKmls)
+    allKmls.forEach((kml) => {
+      const viewport = kml.getDefaultViewport();
+      console.log(viewport);
+      totalBounds = totalBounds.union(viewport);
+    });
+    console.log(totalBounds)
+    map.fitBounds(totalBounds);
+  })
+}
+
+initMap();
